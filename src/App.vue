17fa0a8a5a05efbca9b8c968e6c9c1c68993f5ac
Jay Bhingradiya <jaykumar.vinodbhai@healthplix.com>
2025-07-04 13:37:48 +0530

    Add Tailwind CSS support and update styles in Vue Quiz app

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
 </script>
 
 <template>
-  <header>
-    <div>Header</div>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/quiz">Quiz</RouterLink>
+  <header class="h-[60px] flex justify-between items-center bg-purple-500 text-white p-4">
+    <h1 class="text-2xl font-bold">Vue Quiz App</h1>
+
+    <div class="flex gap-4">
+      <!-- <RouterLink to="/">Home</RouterLink> -->
+      <!-- <RouterLink to="/quiz">Start Quiz</RouterLink> -->
+    </div>
   </header>
 
-  <RouterView />
-
-  <footer>This is Footer</footer>
+  <div class="min-h-[calc(100vh-120px)]">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
